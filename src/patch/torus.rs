f98369e715bd2237470a693c0f7d67fd6da18f91
Jeroen van Maanen <jeroen@sollunae.net>
2026-03-18 21:37:55 +0000

    Improved function `calculate_oblong`

--- src/patch/torus.rs
+++ src/patch/torus.rs
@@ -16,6 +16,7 @@ pub struct PatchTorus<S: State<Gen> + Copy, Gen: Generation, E: Effectors> {
 }
 
 const MAX_PATCH_SIZE: u8 = 14 * 18;
+const SQRT_PATCH_SIZE: u8 = 17;
 
 pub fn new_hexagonal<S: State<Gen> + Copy, Gen: Generation>(
     init: S,
@@ -48,29 +49,50 @@ fn calculate_grid(width: usize, height: usize) -> (usize, usize) {
 }
 
 fn calculate_oblong(long: usize, short: usize) -> (usize, usize) {
-    let sm = (short + 17) / 18;
+    let sps = SQRT_PATCH_SIZE as usize;
+    let sm = (short + sps) / (sps + 1);
     let mut s = sm;
-    let (mut l, mut q) = calculate_footprint(long, short, sm);
+    let (mut l, mut q, mut e) = calculate_footprint(long, short, sm);
     if s > 1 {
-        let (la, qa) = calculate_footprint(long, short, sm - 1);
-        if qa < q {
+        let (la, qa, ea) = calculate_footprint(long, short, sm - 1);
+        if qa < q || (qa == q && ea < e) {
             s = sm - 1;
             l = la;
             q = qa;
+            e = ea;
         }
     }
-    let (lb, qb) = calculate_footprint(long, short, sm + 1);
-    if qb < q { (lb, sm + 1) } else { (l, s) }
+    let (lb, qb, eb) = calculate_footprint(long, short, sm + 1);
+    if qb < q || (qb == q && eb < e) {
+        s = sm + 1;
+        l = lb;
+    }
+    debug!("Patch count: {l} x {s}");
+    (l, s)
 }
 
-fn calculate_footprint(long: usize, short: usize, s: usize) -> (usize, usize) {
-    let sx = (short + s - 1) / s;
-    let lx = (PATCH_SIZE as usize + sx - 1) / sx;
-    let l = (long + lx - 1) / lx;
-    let footprint = (l, s * l);
+fn calculate_footprint(long: usize, short: usize, s: usize) -> (usize, usize, usize) {
+    let sd = if s > 1 { 1 } else { 0 };
+    let sx = sd + (short + s - 1) / s;
+    let lx = (PATCH_SIZE as usize) / sx;
+    let ld = if lx < long { 1 } else { 0 };
+    let l = (long + lx - ld - 1) / (lx - ld);
+    let mut edge = 0;
+    if s > 1 {
+        edge += s * lx;
+    }
+    if l > 1 {
+        edge += l * sx;
+        if s > 1 {
+            edge += 4;
+        }
+    }
+    let footprint = (l, s * l, edge);
+    let se = sx - sd;
+    let le = lx - ld;
     debug!(
-        "Footprint: ({long} / {l}, {short} / {s}) -> ({lx}, {sx}) -> {}",
-        footprint.1
+        "Footprint: ({long} / {l}, {short} / {s}) -> ({le} + {ld}, {se} + {sd}) -> {} / {}",
+        footprint.1, footprint.2
     );
     footprint
 }
